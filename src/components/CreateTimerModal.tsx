d801d2981f8e3285418c19f71edd2d37c163e021
Riccardo P <riccardop@outlook.com>
2024-06-30 12:41:50 +1000

    CreateTimerModal: added HTML code and logic for creating a new timer

--- src/components/CreateTimerModal.tsx
+++ src/components/CreateTimerModal.tsx
@@ -1,10 +1,43 @@
+import { useContext, useRef } from 'react';
+import { DataContextType } from '../types/DataContextType';
+import { DataContext } from '../context/DataContext';
+import { ACTIONS } from '../reducers/Actions';
+import { TimerType } from '../types/TimerType';
+import { FormEvent } from 'react';
 import ReactDOM from 'react-dom';
 
 const CreateTimerModal = ({
   isModalOpen = false,
   onCancel,
 }: CreateTimerModalProps) => {
+  const { dispatch } = useContext<DataContextType>(DataContext);
+  const timerName = useRef<HTMLInputElement>(null);
+  const timerSeconds = useRef<HTMLInputElement>(null);
+
+  const defaultTimerName = 'New Timer';
+  const defaultTimerSeconds = 10;
+
   const modalSelector = document.querySelector('#createTimerModal');
+
+  const createTimer = (ev: FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+
+    const timerLabel = timerName.current?.value || defaultTimerName;
+    const timerValue =
+      Number(timerSeconds.current?.value) || defaultTimerSeconds;
+
+    const newTimer: TimerType = {
+      id: 'd',
+      label: timerLabel,
+      initialTimeStamp: new Date(),
+      initialValue: timerValue,
+      latestTimeStamp: new Date(),
+      latestValue: timerValue,
+      paused: true,
+      completed: false,
+    };
+    dispatch({ type: ACTIONS.CREATE_TIMER, payload: newTimer });
+  };
 
   if (!modalSelector) return;
   if (!isModalOpen) return;
@@ -12,9 +45,30 @@ const CreateTimerModal = ({
   return ReactDOM.createPortal(
     <>
       <h2>Create new timer</h2>
-      <div>Content for creating a new timer</div>
-      <button onClick={onCancel}>Cancel</button>
-      <button>Create timer</button>
+      <form onSubmit={createTimer}>
+        <div>
+          <label htmlFor="timer-name">Timer name</label>
+          <input
+            id="timer-name"
+            type="text"
+            ref={timerName}
+            defaultValue={defaultTimerName}
+          />
+        </div>
+        <div>
+          <label htmlFor="seconds">Number of seconds</label>
+          <input
+            id="seconds"
+            type="number"
+            ref={timerSeconds}
+            defaultValue={defaultTimerSeconds}
+          />
+        </div>
+        <div>
+          <button onClick={onCancel}>Cancel</button>
+          <button>Create timer</button>
+        </div>
+      </form>
     </>,
     modalSelector
   );
