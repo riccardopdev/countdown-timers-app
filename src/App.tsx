474bd3dd43eeae28437b1b44f0c551a8bb5566cf
Riccardo P <riccardop@outlook.com>
2024-06-30 10:39:31 +1000

    Wired up the Reducer and DataContext with the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useReducer } from 'react';
+import Reducer from './reducers/Reducer';
+import { DataContext } from './context/DataContext';
+import { State } from './state/State';
+
 const App = () => {
-  return <h1>App</h1>;
+  const [state, dispatch] = useReducer(Reducer, State);
+  return (
+    <>
+      <DataContext.Provider value={{ state, dispatch }}>
+        <h1>App</h1>
+      </DataContext.Provider>
+    </>
+  );
 };
 
 export default App;
