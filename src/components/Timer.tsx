34e3582a680df640f5febf16bfc6c5379d641f65
Riccardo P <riccardop@outlook.com>
2024-06-30 15:30:57 +1000

    Timer: added logic to update countdown value of timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useContext } from 'react';
 import { TimerType } from '../types/TimerType';
 import TimerControls from './TimerControls';
 import TimerDisplay from './TimerDisplay';
+import { DataContextType } from '../types/DataContextType';
+import { DataContext } from '../context/DataContext';
+import { ACTIONS } from '../reducers/Actions';
 
 const Timer = ({ timer }: TimerProps) => {
+  const { dispatch } = useContext<DataContextType>(DataContext);
+
+  useEffect(() => {
+    if (!timer.paused && !timer.completed) {
+      const timerId = setTimeout(() => {
+        dispatch({ type: ACTIONS.UPDATE_TIMER, payload: timer });
+      }, 1000);
+
+      return () => clearTimeout(timerId);
+    }
+  }, [timer, dispatch]);
+
   return (
     <div>
       <TimerDisplay seconds={timer.latestValue} />
